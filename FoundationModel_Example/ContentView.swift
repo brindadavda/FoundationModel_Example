67afff9eb7006dd93922bd007634d6667a721065
Brinda Davda <brinda@capitalnumbers.com>
2026-04-17 00:29:26 +0530

    first commit

---
+++ FoundationModel_Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoundationModel_Example
+//
+//  Created by Enpointe on 17/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoundationModel_Example/FoundationModel_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModel_ExampleApp.swift
+//  FoundationModel_Example
+//
+//  Created by Enpointe on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModel_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
